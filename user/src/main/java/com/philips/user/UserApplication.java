b2e073549d91527a33a8f5e978fd30131324e21a
QiZhong Lin <qizhong.lin@philips.com>
2016-07-01 11:26:39 +0800

    https://spring.io/guides/gs/client-side-load-balancing/

---
+++ user/src/main/java/com/philips/user/UserApplication.java
@@ -0,0 +1,40 @@
+package com.philips.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by 310031267 on 2016/6/30.
+ */
+
+@SpringBootApplication
+@RestController
+@RibbonClient(name = "say-hello", configuration = SayHelloConfiguration.class)
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam(value="name", defaultValue = "Artaban") String name) {
+        String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
+        return String.format("%s, %s!", greeting, name);
+    }
+}
